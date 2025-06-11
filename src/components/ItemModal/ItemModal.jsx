d17631571a6a479287e45525b6fe6cba28b255bf
voidsky <voidsky02@gmail.com>
2025-06-10 20:07:19 -0700

    finish styling of item-card__modal / no functionality

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -2,6 +2,10 @@ function ItemModal() {
   return (
     <div className="modal modal_opened">
       <div className="modal__container">
+        <button
+          className="modal__close-btn item-modal__close-btn"
+          type="button"
+        ></button>
         <img
           className="item-modal__image"
           src="https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591"
