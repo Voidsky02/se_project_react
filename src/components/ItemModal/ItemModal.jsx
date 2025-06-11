63febab6c3a5b8feb930241983ad396cd17f23e0
voidsky <voidsky02@gmail.com>
2025-06-10 20:41:08 -0700

    will start making the itemCardModal dynamic

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,4 +1,13 @@
 function ItemModal() {
+  const itemModalImage =
+    "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591";
+  const itemModalTitle = "Cap";
+  const itemModalTemp = "Hot";
+
+  //   believe im going to set the state here, then create a function that will alter the state,
+  // I will pass this function the the item cards so that i can attach an event listeners
+  // to the card and run the function whenever it fires, opening and closing the modal
+
   return (
     <div className="modal modal_opened">
       <div className="modal__container">
@@ -6,12 +15,9 @@ function ItemModal() {
           className="modal__close-btn item-modal__close-btn"
           type="button"
         ></button>
-        <img
-          className="item-modal__image"
-          src="https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591"
-        />
-        <h3 className="item-modal__title">Cap</h3>
-        <p className="item-modal__description">Weather: Hot</p>
+        <img className="item-modal__image" src={itemModalImage} />
+        <h3 className="item-modal__title">{itemModalTitle}</h3>
+        <p className="item-modal__description">{`Weather: ${itemModalTemp}`}</p>
       </div>
     </div>
   );
