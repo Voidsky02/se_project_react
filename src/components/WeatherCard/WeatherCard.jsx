dcbe1c5c3d184dbbee40f9d3ad008b6ad5b21f43
voidsky <voidsky02@gmail.com>
2025-07-01 01:17:20 -0700

    delete old weather banner object

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -3,25 +3,6 @@ import { useState, useEffect } from "react";
 function WeatherCard({ temperature, weather, weatherOptions }) {
   const [currentBanner, setCurrentBanner] = useState("");
 
-  const weatherBanners = {
-    day: {
-      clear: "src/images/day-clear.svg",
-      cloudy: "src/images/day-cloudy.svg",
-      rain: "src/images/day-rain.svg",
-      storm: "src/images/day-storm.svg",
-      snow: "src/images/day-snow.svg",
-      fog: "src/images/day-fog.svg",
-    },
-    night: {
-      clear: "src/images/night-clear.svg",
-      cloudy: "src/images/night-cloudy.svg",
-      rain: "src/images/night-rain.svg",
-      storm: "src/images/night-storm.svg",
-      snow: "src/images/night-snow.svg",
-      fog: "src/images/night-fog.svg",
-    },
-  };
-
   // pass it weather.icon[2] & weather.id
   function setWeatherBanner(currentTime, currentWeather) {
     let locationTime = "";
