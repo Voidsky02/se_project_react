21fbbe839ae69c6590d4e2eb05b0b151be7bf653
voidsky <voidsky02@gmail.com>
2025-07-01 00:39:56 -0700

    create weatherOptions as instructed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -47,6 +47,67 @@ const defaultClothingItems = [
   },
 ];
 
-const weatherOptions = {};
+const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../images/day/clear.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../images/day/cloudy.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../images/day/rain.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "storm",
+    url: new URL("../images/day/storm.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../images/day/snow.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../images/day/fog.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../images/night/clear.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../images/night/cloudy.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../images/night/rain.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "storm",
+    url: new URL("../images/night/storm.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../images/night/snow.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../images/night/fog.svg", import.meta.url).href,
+  },
+];
 
-export { weatherApiKey, location, defaultClothingItems };
+export { weatherApiKey, location, defaultClothingItems, weatherOptions };
