a7ed714b91d925fe62fe58441c0f265d2500bb2f
voidsky <voidsky02@gmail.com>
2025-07-14 19:14:13 -0700

    replace div element with section element in ClothingSection container

--- src/components/Profile/ClothesSection/ClothesSection.jsx
+++ src/components/Profile/ClothesSection/ClothesSection.jsx
@@ -3,30 +3,25 @@ import ItemCard from "../../ItemCard/ItemCard";
 
 function ClothesSection({ clothingItems, openClothesModal, handleCardClick }) {
   return (
-    <>
-      <div className="clothes-section__container">
-        <div className="clothes-section__header-container">
-          <h2 className="clothes-section__title">Your items</h2>
-          <button
-            className="clothes-section__add-btn"
-            onClick={openClothesModal}
-          >
-            + Add new
-          </button>
-        </div>
-        <div className="cards__container">
-          {clothingItems.map((item) => {
-            return (
-              <ItemCard
-                key={item._id}
-                item={item}
-                handleCardClick={handleCardClick}
-              />
-            );
-          })}
-        </div>
+    <section className="clothes-section__container">
+      <div className="clothes-section__header-container">
+        <h2 className="clothes-section__title">Your items</h2>
+        <button className="clothes-section__add-btn" onClick={openClothesModal}>
+          + Add new
+        </button>
       </div>
-    </>
+      <div className="cards__container">
+        {clothingItems.map((item) => {
+          return (
+            <ItemCard
+              key={item._id}
+              item={item}
+              handleCardClick={handleCardClick}
+            />
+          );
+        })}
+      </div>
+    </section>
   );
 }
 
