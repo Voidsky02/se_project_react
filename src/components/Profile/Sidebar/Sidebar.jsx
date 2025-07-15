30d7087e4d842683cd09a55c8619aeb1bb09bdf5
voidsky <voidsky02@gmail.com>
2025-07-14 19:22:15 -0700

    use aside element instead of div, in Sidebar.jsx

--- src/components/Profile/Sidebar/Sidebar.jsx
+++ src/components/Profile/Sidebar/Sidebar.jsx
@@ -4,16 +4,14 @@ function Sidebar() {
   const userPicture = "src/images/temp-profile-pic.svg";
 
   return (
-    <>
-      <div className="sidebar__user">
-        <img
-          className="sidebar__user-image"
-          src={userPicture}
-          alt="temporary user picture"
-        />
-        <p className="sidebar__user-name">Terrence Tegegne</p>
-      </div>
-    </>
+    <aside className="sidebar__user">
+      <img
+        className="sidebar__user-image"
+        src={userPicture}
+        alt="temporary user picture"
+      />
+      <p className="sidebar__user-name">Terrence Tegegne</p>
+    </aside>
   );
 }
 
