efbd4f1143d99492d6402cd07411634e5b38462e
voidsky <voidsky02@gmail.com>
2025-12-21 20:17:41 -0700

    fix middleware import path error

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3001";
+const baseUrl = process.env.NODE_ENV === "production" ? "https://api.wtwrVoid.twilightparadox.com" : "http://localhost:3001";
 
 const checkResponse = (res) => {
   return res.ok
